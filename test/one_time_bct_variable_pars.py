284ccf3c11d217429a214191bce4bcdc625a84cf
Saitel Agudelo <saitel.agudelo@gmail.com>
2020-12-01 23:50:37 +0100

    Add test for one-time backward contact tracing algorithm

---
+++ test/one_time_bct_variable_pars.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 17:38:14 2020
+
+@author: saitel
+"""
+import numpy as np
+from parameters import parameters
+from periodic.backward_tracing import one_time as ot
+from helper.plotter import Plotter
+
+
+def variable_parameters_test():
+    """
+    Test one time backward tracing using constant parameters
+
+    Returns
+    -------
+    None.
+
+    """
+    pars = parameters.VariableParameters()
+    bct = ot.OneTimeBCT(pars)
+
+    a_max = 2
+    t_0_max = 6
+
+    t_0 = np.arange(0, t_0_max + pars.h(), pars.h())
+    a = np.arange(0, a_max + pars.h(), pars.h())
+
+    kappa = bct.calculate_kappa(a, t_0)
+    a, t_0 = np.meshgrid(a, t_0)
+
+    return t_0, a, kappa
+
+
+t_0, a, kappa = variable_parameters_test()
+# Plot results
+Plotter.plot_3D(t_0, a, kappa,
+                'figures/periodicity/backward_ct_ot_variable_pars')
