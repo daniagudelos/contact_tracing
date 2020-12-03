1f6b5425466d9e21c403b7de1dee6deb55f229d9
Saitel Agudelo <saitel.agudelo@gmail.com>
2020-12-03 15:19:19 +0100

    Update tests for one time bct

--- test/one_time_bct_constant_pars.py
+++ test/one_time_bct_constant_pars.py
@@ -25,17 +25,79 @@ def constant_parameters_test():
 
     a_max = 2
     t_0_max = 6
+    h = pars.get_h
 
-    t_0 = np.arange(0, t_0_max + pars.h(), pars.h())
-    a = np.arange(0, a_max + pars.h(), pars.h())
+    t_0 = np.arange(0, t_0_max + h(), h())
+    a = np.arange(0, a_max + h(), h())
 
     kappa = bct.calculate_kappa(a, t_0)
     a, t_0 = np.meshgrid(a, t_0)
 
+    # Plot results
+    Plotter.plot_3D(t_0, a, kappa,
+                    '../figures/periodicity/backward_ct_ot_constant_pars')
+
     return t_0, a, kappa
 
 
-t_0, a, kappa = constant_parameters_test()
-# Plot results
-Plotter.plot_3D(t_0, a, kappa,
-                'figures/periodicity/backward_ct_ot_constant_pars')
+def constant_parameters_p0_test():
+    """
+    Test one time backward tracing using constant parameters
+
+    Returns
+    -------
+    None.
+
+    """
+    pars = parameters.ConstantParameters(p=0)
+    bct = ot.OneTimeBCT(pars)
+
+    a_max = 2
+    t_0_max = 6
+    h = pars.get_h
+
+    t_0 = np.arange(0, t_0_max + h(), h())
+    a = np.arange(0, a_max + h(), h())
+
+    kappa = bct.calculate_kappa(a, t_0)
+    a, t_0 = np.meshgrid(a, t_0)
+
+    # Plot results
+    Plotter.plot_3D(t_0, a, kappa,
+                    '../figures/periodicity/backward_ct_ot_constant_pars_p0')
+
+    return t_0, a, kappa
+
+
+def constant_parameters_p1_test():
+    """
+    Test one time backward tracing using constant parameters
+
+    Returns
+    -------
+    None.
+
+    """
+    pars = parameters.ConstantParameters(p=1)
+    bct = ot.OneTimeBCT(pars)
+
+    a_max = 2
+    t_0_max = 6
+    h = pars.get_h
+
+    t_0 = np.arange(0, t_0_max + h(), h())
+    a = np.arange(0, a_max + h(), h())
+
+    kappa = bct.calculate_kappa(a, t_0)
+    a, t_0 = np.meshgrid(a, t_0)
+
+    # Plot results
+    Plotter.plot_3D(t_0, a, kappa,
+                    '../figures/periodicity/backward_ct_ot_constant_pars_p1')
+
+    return t_0, a, kappa
+
+
+#constant_parameters_p0_test()
+#constant_parameters_test()
+constant_parameters_p1_test()
