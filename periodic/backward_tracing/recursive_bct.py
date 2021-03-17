02d0d6211ba677bb82bb69ad7cb5b43b8bafd871
Saitel Agudelo <saitel.agudelo@gmail.com>
2021-03-17 19:05:33 +0100

    Speed up bct calculation using periodicity

--- periodic/backward_tracing/recursive_bct.py
+++ periodic/backward_tracing/recursive_bct.py
@@ -6,31 +6,34 @@ Created on Wed Nov 25 23:54:35 2020
 @author: saitel
 """
 from scipy.integrate import trapz, solve_ivp
-from parameters.parameters import ConstantParameters, VariableParameters, TestParameters1
+from parameters.parameters import TestParameters1
 import numpy as np
 from helper.plotter import Plotter
 
 
 class RecursiveBCT:
     def __init__(self, parameters, a_max, t_0_max):
-        self.period = parameters.get_period()
-        self.period_length = parameters.get_period_length()
-        self.beta = parameters.get_beta
-        self.dbeta = parameters.get_dbeta
-        self.mu = parameters.get_mu
-        self.sigma = parameters.get_sigma
-        self.p = parameters.get_p
-        self.h = parameters.get_h
+        self.parameters = parameters
+        self.period = self.parameters.get_period()  # Period in days
+        self.period_length = self.parameters.get_period_length()
+        self.beta = self.parameters.get_beta
+        self.dbeta = self.parameters.get_dbeta
+        self.mu = self.parameters.get_mu
+        self.sigma = self.parameters.get_sigma
+        self.p = self.parameters.get_p
+        self.h = self.parameters.get_h
+
         self.a_max = a_max
+        self.a_length = int(round(a_max / self.h(), 1))
+        self.a_array = np.linspace(0.0, self.a_max, self.a_length + 1)
+
         self.t_0_max = t_0_max
         self.t_0_length = int(round(t_0_max / self.h(), 1))
-        self.a_length = int(round(a_max / self.h(), 1))
         self.t_0_array = np.linspace(0.0, self.t_0_max + self.a_max,
                                      self.t_0_length + self.a_length + 1)
-        self.a_array = np.linspace(0.0, self.a_max, self.a_length + 1)
+
         self.kappa_minus = np.ones((self.t_0_length + self.a_length + 1,
                                     self.a_length + 1))
-        self.dkappa_minus = np.ones_like(self.kappa_minus)
 
     def integrand(self, b_index, t_0_index, a_index):
         return (self.kappa_minus[t_0_index + a_index - b_index, b_index] * (
@@ -71,29 +74,62 @@ class RecursiveBCT:
                          + self.p() * self.integral(a, t_0_index)))
         return dkappa_minus
 
-    def calculate_kappa_minus_for_cohort(self, t_0_index, a_index):
-        a_array = self.a_array[0:a_index + 1]  # from 0 to a_index
-        kappa0 = [1]  # must be a 1-d array!
-        sol = solve_ivp(self.fun, [0, a_array[-1]], kappa0, method='Radau',
-                        t_eval=a_array, dense_output=True, vectorized=True,
-                        args=[t_0_index], rtol=1e-4, atol=1e-9)
+    def calculate_kappa_minus_for_cohort(self, t_0_index, a_start, a_end):
+        a_array = self.a_array[a_start:a_end + 1]  # from 0 to a_end
+        kappa0 = [self.kappa_minus[t_0_index, a_start]]  # must be a 1-d array!
+        sol = solve_ivp(self.fun, [a_array[0], a_array[-1]], kappa0,
+                        method='Radau', t_eval=a_array, dense_output=True,
+                        vectorized=True, args=[t_0_index], rtol=1e-4,
+                        atol=1e-9)
         return sol
 
     def calculate_kappa_minus(self):
-        # Calculate ghost cohorts: i represents t_0 + a index
-        for t_0_index in range(len(self.t_0_array) - 2, self.t_0_length, -1):
-            # From t_0 + a - h to t_0 + h
-            # skip last t_0 ghost because kappa[-1,:] = 1
-            a_index = self.t_0_length + self.a_length - t_0_index
+        a_periods = int(self.a_max / self.period)
+        # One extra periods for ghost cells
+        t_0_periods = int(self.t_0_max / self.period) + a_periods
 
-            sol = self.calculate_kappa_minus_for_cohort(t_0_index, a_index)
-            self.kappa_minus[t_0_index, 0: a_index + 1] = sol.y.reshape(-1)
+        # Calculate ghost cohorts: i represents t_0 + T
+        start_ghost = self.period_length - 1
+        # skip last t_0 ghost because kappa[-1,:] = 1 & not needed
+        end_ghost = 2 * self.period_length - 1
 
-        for t_0_index in range(self.t_0_length, -1, -1):  # From t_0  to 0
-            a_index = self.a_length
+        for period_index in range(1, a_periods + 1):
+            a_start = self.period_length * (period_index - 1)
+            a_end = self.period_length * (period_index - 1)
 
-            sol = self.calculate_kappa_minus_for_cohort(t_0_index, a_index)
-            self.kappa_minus[t_0_index, 0: a_index + 1] = sol.y.reshape(-1)
+            # Calculate upper-left half of matrix second period
+            for t_0_index in range(end_ghost, start_ghost, -1):
+                a_end += 1
+                # From 2 T to T + h
+                sol = self.calculate_kappa_minus_for_cohort(t_0_index, a_start,
+                                                            a_end)
+                self.kappa_minus[t_0_index, a_start: a_end + 1] = (
+                    sol.y.reshape(-1))
+
+            # Copy values to first period in t_0 axis
+            # a_end here should be = self.period_length * period_index
+            self.kappa_minus[0: self.period_length, a_start:a_end + 1] = (
+                self.kappa_minus[self.period_length: 2 * self.period_length,
+                                 a_start:a_end + 1])
+
+            # Calculate lower-right half of matrix period: From T  to 1
+            for t_0_index in range(self.period_length - 1, 0, -1):
+                a_start += 1
+                sol = self.calculate_kappa_minus_for_cohort(t_0_index, a_start,
+                                                            a_end)
+                self.kappa_minus[t_0_index, a_start: a_end + 1] = (
+                    sol.y.reshape(-1))
+
+            a_start = self.period_length * (period_index - 1)
+            a_end = self.period_length * (period_index)
+
+            # Copy values to the rest of the periods in t_0-axis
+            for i in range(1, t_0_periods):  # 1 : t_0_periods - 1
+                t_0_start = self.period_length * i + 1
+                t_0_end = self.period_length * (i + 1) + 1
+                self.kappa_minus[t_0_start: t_0_end, a_start:a_end + 1] = (
+                    self.kappa_minus[1: self.period_length + 1,
+                                     a_start:a_end + 1])
 
         # Fix numerical errors:
         self.kappa_minus = np.where(self.kappa_minus < 0, 0, self.kappa_minus)
@@ -105,10 +141,10 @@ class RecursiveBCT:
 def recursive_bct_test(pars, filename, a_max=2, t_0_max=6):
     otbct = RecursiveBCT(pars, a_max, t_0_max)
     t_0_array, a_array, kappa_minus = otbct.calculate_kappa_minus()
-    # a, t_0 = np.meshgrid(a_array, t_0_array)
-    # Plotter.plot_3D(t_0, a, kappa_minus, filename + '_60_10', my=0.5)
-    # Plotter.plot_3D(t_0, a, kappa_minus, filename + '_n60_10', azim=-60,
-    #                my=0.5)
+    a, t_0 = np.meshgrid(a_array, t_0_array)
+    Plotter.plot_3D(t_0, a, kappa_minus, filename + '_60_10', my=0.5)
+    Plotter.plot_3D(t_0, a, kappa_minus, filename + '_n60_10', azim=-60,
+                    my=0.5)
     return t_0_array, a_array, kappa_minus
 
 
@@ -118,43 +154,43 @@ def main3():
                       3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1])
     par = TestParameters1(beta2, p=1/3, h=0.25, period_time=T)
     t_0_array, a_array, kappa_plus = recursive_bct_test(
-        par, '../../figures/periodic/fct_re_variable_p03', a_max=3 * T,
-        t_0_max=T)
+        par, '../../figures/periodic/fct_re_variable_p03', a_max=2 * T,
+        t_0_max=3*T)
     return t_0_array, a_array, kappa_plus
 
 
-def main2():
-    t_0_array, a_array, kappa_minus = recursive_bct_test(VariableParameters(
-        p=1/3, h=0.1), '../../figures/periodic/bct_re_variable_p03', 2, 2)
-    return t_0_array, a_array, kappa_minus
+# def main2():
+#     t_0_array, a_array, kappa_minus = recursive_bct_test(VariableParameters(
+#         p=1/3, h=0.1), '../../figures/periodic/bct_re_variable_p03', 2, 2)
+#     return t_0_array, a_array, kappa_minus
 
 
-def main():
-    print('Running simulation BCT with constant parameters and p=0.0')
-    recursive_bct_test(ConstantParameters(p=0, h=0.5),
-                       '../../figures/non_periodic/bct_re_constant_p0')
-    print('Running simulation BCT with constant parameters and p=1/3')
-    recursive_bct_test(ConstantParameters(p=1/3, h=0.5),
-                       '../../figures/periodic/bct_re_constant_p03')
-    print('Running simulation BCT with constant parameters and p=2/3')
-    recursive_bct_test(ConstantParameters(p=2/3, h=0.5),
-                       '../../figures/periodic/bct_re_constant_p06')
-    print('Running simulation BCT with constant parameters and p=1')
-    recursive_bct_test(ConstantParameters(p=1, h=0.5),
-                       '../../figures/periodic/bct_re_constant_p1')
+# def main():
+#     print('Running simulation BCT with constant parameters and p=0.0')
+#     recursive_bct_test(ConstantParameters(p=0, h=0.5),
+#                        '../../figures/non_periodic/bct_re_constant_p0')
+#     print('Running simulation BCT with constant parameters and p=1/3')
+#     recursive_bct_test(ConstantParameters(p=1/3, h=0.5),
+#                        '../../figures/periodic/bct_re_constant_p03')
+#     print('Running simulation BCT with constant parameters and p=2/3')
+#     recursive_bct_test(ConstantParameters(p=2/3, h=0.5),
+#                        '../../figures/periodic/bct_re_constant_p06')
+#     print('Running simulation BCT with constant parameters and p=1')
+#     recursive_bct_test(ConstantParameters(p=1, h=0.5),
+#                        '../../figures/periodic/bct_re_constant_p1')
 
-    print('Running simulation BCT with variable parameters and p=0.0')
-    recursive_bct_test(VariableParameters(p=0, h=0.5),
-                       '../../figures/non_periodic/bct_re_variable_p0')
-    print('Running simulation BCT with variable parameters and p=1/3')
-    recursive_bct_test(VariableParameters(p=1/3, h=0.5),
-                       '../../figures/periodic/bct_re_variable_p03')
-    print('Running simulation BCT with variable parameters and p=2/3')
-    recursive_bct_test(VariableParameters(p=2/3, h=0.5),
-                       '../../figures/periodic/bct_re_variable_p06')
-    print('Running simulation BCT with variable parameters and p=1')
-    recursive_bct_test(VariableParameters(p=1, h=0.5),
-                       '../../figures/periodic/bct_re_variable_p1')
+#     print('Running simulation BCT with variable parameters and p=0.0')
+#     recursive_bct_test(VariableParameters(p=0, h=0.5),
+#                        '../../figures/non_periodic/bct_re_variable_p0')
+#     print('Running simulation BCT with variable parameters and p=1/3')
+#     recursive_bct_test(VariableParameters(p=1/3, h=0.5),
+#                        '../../figures/periodic/bct_re_variable_p03')
+#     print('Running simulation BCT with variable parameters and p=2/3')
+#     recursive_bct_test(VariableParameters(p=2/3, h=0.5),
+#                        '../../figures/periodic/bct_re_variable_p06')
+#     print('Running simulation BCT with variable parameters and p=1')
+#     recursive_bct_test(VariableParameters(p=1, h=0.5),
+#                        '../../figures/periodic/bct_re_variable_p1')
 
 
 if __name__ == '__main__':
